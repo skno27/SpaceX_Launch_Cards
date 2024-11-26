f4a7e0d5f0281df488797d78a378e228f60aba6d
Deshon Morgan <deshonmorgan1317@gmail.com>
2024-11-25 23:39:30 -0600

    Initial commit, with Read Me

---
+++ README.md
@@ -0,0 +1,7 @@
+# Most Recent
+
+I have finally finished this application and am excited to share it with others. It is now complete and ready for use.
+
+## About
+
+This project is a Launch Card Flask Application designed to display SpaceX launch data in a convenient and user-friendly way. The application fetches data from the SpaceX API and uses a macro-based HTML template to render the launch information dynamically. It provides an organized and visually appealing interface for users to explore the latest SpaceX launches.
---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests
+
+app = Flask(__name__)  # init flask app?
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", launches=launches)
+
+
+@app.template_filter("date_only")
+def date_only_filter(s):
+    date_object = datetime.strptime(s, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return date_object.date()
+
+
+# load data using SpaceX API
+def fetch_space_launches():
+    url = "https://api.spacexdata.com/v5/launches"
+    response = requests.get(url)
+    # if successful ...
+    if response.status_code == 200:
+        launches = response.json()
+        launches.sort(key=lambda x: x["date_utc"], reverse=True)
+        return launches
+    else:
+        return []
+
+
+def categorize_launches(launches):
+    # filter launches such that each launch has the key  success and not the key upcoming...
+    successful = list(filter(lambda x: x["success"] and not x["upcoming"], launches))
+    failed = list(filter(lambda x: not x["success"] and not x["upcoming"], launches))
+    upcoming = list(filter(lambda x: x["upcoming"], launches))
+
+    return {"successful": successful, "failed": failed, "upcoming": upcoming}
+
+
+launches = categorize_launches(fetch_space_launches())
+
+# print(launches)
+
+
+# run only if we run THIS file..
+if __name__ == "__main__":
+    app.run(debug=True)  # update whenever we save , then refresh page
